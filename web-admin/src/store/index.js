12005cbf82489a3296599073c141f19e058055c4
nur mizwari <nurmizwari04@gmail.com>
2022-10-07 22:02:40 +0700

    membuat daftarrr hadir button belum jalan

--- web-admin/src/store/index.js
+++ web-admin/src/store/index.js
@@ -1,19 +1,9 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-
-function counterReducer(state = { value: 0 }, action) {
-  switch (action.type) {
-    case "counter/incremented":
-      return { value: state.value + 1 };
-    case "counter/decremented":
-      return { value: state.value - 1 };
-    default:
-      return state;
-  }
-}
+import teacherReducer from "./reducer/teacher";
 
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
-let store = createStore(counterReducer, applyMiddleware(thunk));
+let store = createStore(teacherReducer, applyMiddleware(thunk));
 
 export default store;
