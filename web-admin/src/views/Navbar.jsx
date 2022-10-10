9af5514d1a58807112cdd7d5cd0e50b4ee06e9c4
muhgifariakbar <muhgifariakbar@gmail.com>
2022-10-10 13:29:57 +0700

    push for nur

--- web-admin/src/views/Navbar.jsx
+++ web-admin/src/views/Navbar.jsx
@@ -52,15 +52,6 @@ export default function Navbar() {
                 <Link
                   class="nav-link active"
                   aria-current="page"
-                  to={"/uploadTugas"}
-                >
-                  Upload Tugas
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link
-                  class="nav-link active"
-                  aria-current="page"
                   to={"/daftarHadir"}
                 >
                   Daftar Hadir
