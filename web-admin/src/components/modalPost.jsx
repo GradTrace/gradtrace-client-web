a10e18af7a9f16e413ec36a89b27377037f76857
muhgifariakbar <muhgifariakbar@gmail.com>
2022-10-12 22:44:15 +0700

    post done

--- web-admin/src/components/modalPost.jsx
+++ web-admin/src/components/modalPost.jsx
@@ -87,71 +87,104 @@ export default function ModalPost(props) {
       ...formAdd,
       [name]: value,
     });
-
-    return (
-      <Modal
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={showModalPost}
-        onHide={onHide}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-center">
-            Add Student Score Here
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <form onSubmit={submit}>
-            <div class="field">
-              <label className="label">Nilai UTS</label>
-              <input
-                class="form-control"
-                placeholder="Nilai UTS"
-                name="UTS"
-                value={formAdd.UTS.score}
-                onChange={addHandler}
-              />
-            </div>
-            <div class="field">
-              <label className="label">Nilai UAS</label>
-              <input
-                class="form-control"
-                placeholder="Nilai UAS"
-                name="UAS"
-                value={formAdd.UAS.score}
-                onChange={addHandler}
-              />
-            </div>
-            <div class="field">
-              <label className="label">Nilai Ulangan 1</label>
-              <input
-                class="form-control"
-                placeholder="Nilai UL1"
-                name="UL1"
-                value={formAdd.UL1.score}
-                onChange={addHandler}
-              />
-            </div>
-            <div class="field">
-              <label className="label">Nilai Ulangan 2</label>
-              <input
-                class="form-control"
-                placeholder="Nilai UL2"
-                name="UL2"
-                value={formAdd.UL2.score}
-                onChange={addHandler}
-              />
-            </div>
-            <div className="field">
-              <button type="submit" class="button is-success">
-                Submit
-              </button>
-              <button class="button">Cancel</button>
-            </div>
-          </form>
-        </Modal.Body>
-      </Modal>
-    );
   };
+
+  console.log(formAdd, "data setelah di submit");
+  return (
+    <Modal
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      show={showModalPost}
+      onHide={onHide}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-center">
+          Add Student Score Here
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form onSubmit={submit}>
+          <div class="field">
+            <label className="label">Nilai UTS</label>
+            <input
+              class="form-control"
+              placeholder="Nilai UTS"
+              name="UTS"
+              value={formAdd.UTS.score}
+              onChange={(e) => {
+                setFormAdd({
+                  ...formAdd,
+                  UTS: {
+                    ...formAdd.UTS,
+                    score: e.target.value,
+                  },
+                });
+              }}
+            />
+          </div>
+          <div class="field">
+            <label className="label">Nilai UAS</label>
+            <input
+              class="form-control"
+              placeholder="Nilai UAS"
+              name="UAS"
+              value={formAdd.UAS.score}
+              onChange={(e) => {
+                setFormAdd({
+                  ...formAdd,
+                  UAS: {
+                    ...formAdd.UAS,
+                    score: e.target.value,
+                  },
+                });
+              }}
+            />
+          </div>
+          <div class="field">
+            <label className="label">Nilai Ulangan 1</label>
+            <input
+              class="form-control"
+              placeholder="Nilai UL1"
+              name="UL1"
+              value={formAdd.UL1.score}
+              onChange={(e) => {
+                setFormAdd({
+                  ...formAdd,
+                  UL1: {
+                    ...formAdd.UL1,
+                    score: e.target.value,
+                  },
+                });
+              }}
+            />
+          </div>
+          <div class="field">
+            <label className="label">Nilai Ulangan 2</label>
+            <input
+              class="form-control"
+              placeholder="Nilai UL2"
+              name="UL2"
+              value={formAdd.UL2.score}
+              onChange={(e) => {
+                setFormAdd({
+                  ...formAdd,
+                  UL2: {
+                    ...formAdd.UL2,
+                    score: e.target.value,
+                  },
+                });
+              }}
+            />
+          </div>
+          <div className="field">
+            <button type="submit" class="button is-success">
+              Submit
+            </button>
+            <button class="button">Cancel</button>
+          </div>
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
 }
