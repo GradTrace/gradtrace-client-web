be88ae3027b1529bef0f27dadd5b8200027e4b9a
ryrulobo <ryru.lobo@gmail.com>
2022-10-13 14:02:36 +0700

    deploy web

--- web-admin/src/router/index.js
+++ web-admin/src/router/index.js
@@ -35,6 +35,10 @@ const router = createBrowserRouter([
             path: "/daftarHadir",
             element: <DaftarHadirPage />,
           },
+          {
+            path: "/",
+            element: <DaftarTugas />,
+          },
         ],
       },
     ],
